e36618f50fcd98f31ae084f63df2fa8bfa908ac7
Vinay <vnypro98@gmail.com>
2024-09-13 22:11:19 +0530

    made few changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import Navbar from './components/Navbar'
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
-    <div>  
-<Navbar/>
-      
+    <div>
+      <Navbar />
+      <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+  return (
+    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, nobis.</div>
+      <div className="w-full md:w-1/2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor, officiis repudiandae et accusamus sit dolorum porro velit maiores voluptas labore!</div>
+    </div>
+  );
+};
+
+export default Banner;
