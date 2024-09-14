b98b979b7ac1c091465b7bb30a0dd098026b80a9
Vinay <vnypro98@gmail.com>
2024-09-14 22:01:46 +0530

    made few changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Footer/>
     </div>
   );
 };
