1aa06610bd8c693e95cc5e3ef630d058b066940d
Vinay <vnypro98@gmail.com>
2024-10-16 23:19:38 +0530

    added backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,39 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is working");
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("Server is running");
-});
+dotenv.config();
 
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+// defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
 
 /*
 const express = require("express");
